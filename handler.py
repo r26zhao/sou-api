1c2858c4511e672ed2eac29bb5e49bae29184bd5
Aaron Zhao <rudy710@qq.com>
2019-07-10 11:36:56 +0800

    async io

--- handler.py
+++ handler.py
@@ -1,8 +1,8 @@
 import asyncio
 import requests
 import time
-import os
 
+from aiohttp import ClientSession
 from local_settings import *
 
 BASE_URL = "https://api.litmos.com/v1.svc"
@@ -25,44 +25,45 @@ async def get_course_data(url, email, headers, info_list):
     RC_completed = 0
     RC_total = 0
     try:
-        courses_request = requests.get(url, headers=headers)
-        if courses_request.status_code != 200:
-            # log
-            pass
-        else:
-            courses_list = courses_request.json()
-            for course in courses_list:
-                if course['Name'].startswith(SAIC):
-                    SAIC_total += 1
-                    if course['Complete']:
-                        SAIC_completed += 1
-                elif course['Name'].startswith(MG):
-                    MG_total += 1
-                    if course['Complete']:
-                        MG_completed += 1
-                elif course['Name'].startswith(RC):
-                    RC_total += 1
-                    if course['Complete']:
-                        RC_completed += 1
-                else:
+        async with ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                if response.status != 200:
+                    # log
                     pass
-        info[SAIC + '_completed'] = SAIC_completed
-        info[SAIC + '_total'] = SAIC_total
+                else:
+                    courses_list = await response.json()
+                    for course in courses_list:
+                        if course['Name'].startswith(SAIC):
+                            SAIC_total += 1
+                            if course['Complete']:
+                                SAIC_completed += 1
+                        elif course['Name'].startswith(MG):
+                            MG_total += 1
+                            if course['Complete']:
+                                MG_completed += 1
+                        elif course['Name'].startswith(RC):
+                            RC_total += 1
+                            if course['Complete']:
+                                RC_completed += 1
+                        else:
+                            pass
+                info[SAIC + '_completed'] = SAIC_completed
+                info[SAIC + '_total'] = SAIC_total
 
-        info[MG + '_completed'] = MG_completed
-        info[MG + '_total'] = MG_total
+                info[MG + '_completed'] = MG_completed
+                info[MG + '_total'] = MG_total
 
-        info[RC + '_completed'] = RC_completed
-        info[RC + '_total'] = RC_total
+                info[RC + '_completed'] = RC_completed
+                info[RC + '_total'] = RC_total
 
-        info_list.append(info)
+                info_list.append(info)
     except Exception as e:
         # log
         pass
 
 
 async def get_data():
-    user_url = BASE_URL + "/users" + SUFFIX
+    user_url = BASE_URL + "/users" + SUFFIX + "&start={}&limit=50"
     user_courses_url = BASE_URL + "/users/{user_id}/courses" + SUFFIX
     header = {
         "apikey": API_KEY,
@@ -71,10 +72,11 @@ async def get_data():
     finished = False
     result = {}
     users_retry = 0
+    info_list = []
+    offset = 0
     while not finished:
-        info_list = []
         try:
-            users_request = requests.get(user_url, headers=header)
+            users_request = requests.get(user_url.format(offset), headers=header)
             if users_request.status_code != 200:
                 if users_retry > 4:
                     # log
@@ -85,20 +87,24 @@ async def get_data():
                     continue
             users_response = users_request.json()
         except Exception as e:
-            # log
+            print(e)
             break
 
         tasks = [asyncio.create_task(get_course_data(
             user_courses_url.format(user_id=user_data['Id']),
-            user_data['Email'],
+            user_data['UserName'],
             headers=header,
             info_list=info_list
         )) for user_data in users_response]
         for task in tasks:
             await task
-        result = {'infoList': info_list}
-        finished = True
 
+        if len(users_response) == 50:
+            offset += 50
+            finished = False
+        else:
+            finished = True
+    result = {'infoList': info_list}
     return result
 
 
@@ -125,8 +131,9 @@ def send_data(data):
 
 if __name__ == '__main__':
     print("start to fetch data from LMS ...")
+    start = time.time()
     data = asyncio.run(get_data())
+    print("cost: {}".format(time.time() - start))
     print("start to send data to CRM ...")
-    send_data(data)
+    #send_data(data)
     print("Done")
-
